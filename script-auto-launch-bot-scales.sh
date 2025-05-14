45bdbb2f0ff866df61aebcb737c41079eaef4391
AssianDE <assian.kando@gmail.com>
2025-05-14 19:58:13 +0200

    feat: script auto launch

--- script-auto-launch-bot-scales.sh
+++ script-auto-launch-bot-scales.sh
@@ -3,7 +3,8 @@
 # Lancer le bot DesMana pour Scales
 cd /home/leo/bots-disord/bot-disc-scales
 source .venv/bin/activate
-nohup python3 main.py &
+nohup python3 main.py > nohuplog 2>&1 &
+echo $! > /home/leo/bots-disord/bot-disc-scales/bot-scales.pid
 
 # Sortir du venv
 deactivate
